84a6efde55aeccbfe59133fef39ff279bbacf993
Rajatt09 <rajatbhati9559@gmail.com>
2023-12-28 13:34:32 +0530

    added background

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Part from './Part/Part'
-import Navbar from './Components/NavBar/NavBar'
-import Footer from './Components/Footer/Footer'
-import HomePage from './Components/HomePage/HomePage'
-import CountdownTimer from './Part/CountdownTimer'
-import About from './Components/About/About'
-import Smoke from './Part/Smoke'
-import Speaker from './Components/Speakers/Speaker'
-import Card1 from './Part/Card1'
-import Timeline from './Components/TimeLine/Timeline'
+// import Navbar from './Components/NavBar/NavBar'
+// import Footer from './Components/Footer/Footer'
+// import HomePage from './Components/HomePage/HomePage'
+// import CountdownTimer from './Part/CountdownTimer'
+// import About from './Components/About/About'
+// import Smoke from './Part/Smoke'
+// import Speaker from './Components/Speakers/Speaker'
+// import Card1 from './Part/Card1'
+// import Timeline from './Components/TimeLine/Timeline'
 
 function App() {
 
@@ -21,15 +21,15 @@ function App() {
 
  
 
-   {/* <Part /> */}
-   <HomePage />
+   <Part />
+   {/* <HomePage /> */}
    {/* <About /> */}
    {/* <Smoke /> */}
    {/* <Speaker /> */}
    {/* <Timeline /> */}
    {/* <Card1 /> */}
-   <Navbar />
-   <Footer />
+   {/* <Navbar /> */}
+   {/* <Footer /> */}
    </>
   )
 }
